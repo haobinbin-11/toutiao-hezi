9fe013a08fa123d56332f0f8632fcb2cbabd13b1
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-04-21 02:24:15 +0800

    Vue-登录-页面布局-使用from表单

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 加载登录组件
+// 在 VueCli 创建的项目中 @ 表示 src 目录
+// 它是src 目录的路径别名
+// 好处就是 它不受当前路径影响
+// 注意 @就是 src 路径 后面别忘了写那个斜杠
+// 使用建议   如果加载的资源路径就在当前目录下 正常写
+//        如果需要进行父级路径查找的 都使用 @
 import Login from '@/views/login'
 
 Vue.use(VueRouter)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="login-container">登录页面</div>
+<!-- el-from表单组件
+  每个表单都必须使用 el-from-item 组件包裹
+  -->
+  <div class="login-container">
+    <el-form ref="form" :model="user" label-width="80px">
+  <el-form-item label="手机号">
+    <el-input v-model="user.name"></el-input>
+  </el-form-item>
+  <el-form-item label="验证码">
+  <el-input v-model="user.code"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">登录</el-button>
+  </el-form-item>
+</el-form>
+  </div>
 </template>
 <script>
 export default {
@@ -8,6 +23,22 @@ export default {
   props: {},
   data () {
     return {
+      user: {
+        name: '',
+        code: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
     }
   }
 
