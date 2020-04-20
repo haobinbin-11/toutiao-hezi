ee79ccafe4381363c05e6099999765f517cb2df1
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-04-20 17:05:44 +0800

    项目初始化-Element-导入到项目中

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 加载element样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 加载全局样式
 import './styles/insex.less'
+// 全局注册element组件
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 // 创建Vue根实例
 // 把router 配置到根实例
