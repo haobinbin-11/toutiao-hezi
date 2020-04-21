cc7d73ecd5f801078ae8bc8feddfbf6d08c399cf
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-04-22 01:56:15 +0800

    关于图标的使用-iconfont字体图标使用

--- src/views/layout/componets/aside.vue
+++ src/views/layout/componets/aside.vue
@@ -1,39 +1,40 @@
 <template>
     <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
+      default-active="/"
       @open="handleOpen"
       @close="handleClose"
-      background-color="#545c64"
+      background-color="#002033"
       text-color="#fff"
       active-text-color="#ffd04b"
-      router>
+      router
+      class="nav-menu">
+        <div class="bj"></div>
       <el-menu-item index="/">
-        <i class="el-icon-document"></i>
+        <i class="iconfont icon-home"></i>
         <span slot="title">首页</span>
       </el-menu-item>
       <el-menu-item index="/article">
-        <i class="el-icon-menu"></i>
+        <i class="iconfont icon-content"></i>
         <span slot="title">内容管理</span>
       </el-menu-item>
       <el-menu-item index="/image">
-        <i class="el-icon-document"></i>
+        <i class="iconfont icon-img"></i>
         <span slot="title">素材管理</span>
       </el-menu-item>
       <el-menu-item index="/publish">
-        <i class="el-icon-setting"></i>
+        <i class="iconfont icon-fabu-"></i>
         <span slot="title">发布文章</span>
       </el-menu-item>
       <el-menu-item index="/comment">
-        <i class="el-icon-setting"></i>
+        <i class="iconfont icon-pinglun"></i>
         <span slot="title">评论管理</span>
       </el-menu-item>
       <el-menu-item index="/fans">
-        <i class="el-icon-setting"></i>
+        <i class="iconfont icon-app"></i>
         <span slot="title">粉丝管理</span>
       </el-menu-item>
       <el-menu-item index="/settings">
-        <i class="el-icon-setting"></i>
+        <i class="iconfont icon-profilesetting"></i>
         <span slot="title">个人设置</span>
       </el-menu-item>
     </el-menu>
@@ -81,5 +82,13 @@ export default {
 }
 .main {
   background-color: #e9eef3;
+}
+.bj {
+  background-image: url(./logo_admin.png) no-repeat;
+}
+.nav-menu {
+  .iconfont {
+    margin-right: 10px;
+  }
 }
 </style>>
