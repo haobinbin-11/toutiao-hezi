8844c2b8bd4c2b1dd1f507c6b5aebb5cd1893d11
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-04-21 15:26:47 +0800

    用户登录-实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,20 +3,25 @@
   每个表单都必须使用 el-from-item 组件包裹
   -->
   <div class="login-container">
-    <el-form ref="form" :model="user" label-width="80px">
-  <el-form-item label="手机号">
-    <el-input v-model="user.name"></el-input>
+    <el-form class="login-style" ref="form" :model="user">
+      <div class="login-header"></div>
+  <el-form-item class="top">
+    <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
   </el-form-item>
-  <el-form-item label="验证码">
-  <el-input v-model="user.code"></el-input>
+  <el-form-item >
+  <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
   </el-form-item>
   <el-form-item>
-    <el-button type="primary" @click="onSubmit">登录</el-button>
+    <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+  </el-form-item>
+  <el-form-item>
+    <el-button class="login-btn" type="primary" @click="onLoginin">登录</el-button>
   </el-form-item>
 </el-form>
   </div>
 </template>
 <script>
+import request from '@/utils/request'
 export default {
   name: 'LoginIndex',
   components: {},
@@ -24,27 +29,67 @@ export default {
   data () {
     return {
       user: {
-        name: '',
-        code: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      }
+        mobile: '',
+        code: ''
+      },
+      checked: true // 是否同意协议的选中状态
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    onLoginin () {
+      // 获取表单数据(根据接口要求绑定数据)
+      const user = this.user
+      // 表单验证
+      // 验证通过,提交登录
+      request({
+        method: 'POST',
+        url: '/mp/v1_0/authorizations',
+        // data用来设置请求体
+        data: user
+      }).then(res => {
+        // 登录成功
+        console.log(res)
+      }).catch(err => {
+        // 登录失败
+        console.log('登录失败', err)
+      })
+      // 处理后端响应结果
+      // 成功:XXX
+      // 失败:XXX
     }
   }
 
 }
 </script>
 
-<style>
-
-</style>
+<style lang="less" scoped>
+.login-container{
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url("./login_bg.jpg") no-repeat;
+  background-size: cover;
+}
+.login-style{
+    background-color: #fff;
+    padding: 50px;
+    min-width: 300px;
+}
+.login-btn{
+  width: 100%;
+}
+.login-header{
+  height: 57px;
+  width: 300px;
+  background: url("./logo_index.png") no-repeat;
+}
+.top{
+  margin-top: 20px;
+}
+</style>>
