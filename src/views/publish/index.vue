d3ec6c4d9541b8fb531b4a63dbbdf37a5cc90bc5
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-04-26 01:33:26 +0800

    发布文章-表单数据绑定(文章频道处理)-

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -8,25 +8,24 @@
 </el-breadcrumb>
   </div>
   <div class="text item">
-<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-  <el-form-item label="标题" prop="name">
+<el-form :model="article" :rules="rules" ref="article" label-width="100px" class="demo-ruleForm">
+  <el-form-item label="标题" prop="title">
     <el-input v-model="article.title"></el-input>
   </el-form-item>
-  <el-form-item label="内容" prop="desc">
+  <el-form-item label="内容" prop="content">
     <el-input type="textarea" v-model="article.content"></el-input>
   </el-form-item>
-    <el-form-item label="封面" prop="resource">
-    <el-radio-group v-model="ruleForm.resource">
-      <el-radio label="单图"></el-radio>
-      <el-radio label="三图"></el-radio>
-      <el-radio label="无图"></el-radio>
-      <el-radio label="自动"></el-radio>
+    <el-form-item label="封面" prop="cover">
+    <el-radio-group v-model="article.cover.type">
+      <el-radio :label="1">单图</el-radio>
+      <el-radio :label="3">三图</el-radio>
+      <el-radio :label="0">无图</el-radio>
+      <el-radio :label="-1">自动</el-radio>
     </el-radio-group>
   </el-form-item>
-   <el-form-item label="频道" prop="region">
-    <el-select v-model="ruleForm.region" placeholder="请选择">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
+   <el-form-item label="频道" prop="channel_id">
+    <el-select v-model="article.channel_id" placeholder="请选择">
+      <el-option :label="item.name" v-for="(item, index) in channels" :key="index" :value="item.id"></el-option>
     </el-select>
   </el-form-item>
   <el-form-item>
@@ -40,39 +39,34 @@
 </template>
 
 <script>
+import { getArticleChannels } from '@/APi/article'
 export default {
   name: 'PublishIndex',
   components: {},
   props: {},
   data () {
     return {
-      ruleForm: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
+      channels: [], // 文章频道列表
       article: {
         title: '', // 文章标题
         content: '', // 文章内容
         cover: { // 文章封面
           type: 0, // 封面类型 -1 自动 0 无图 1 1张 3 3张
           images: [] // 图片地址
-        }
+        },
+        channelss: '',
+        chanel_id: null,
+        chanel_name: null
       },
       rules: {
-        name: [
+        title: [
           { required: true, message: '请输入标题', trigger: 'blur' },
-          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          { min: 3, max: 20, message: '长度在 3 到 30 个字符', trigger: 'blur' }
         ],
-        region: [
+        channel_id: [
           { required: true, message: '请选择', trigger: 'change' }
         ],
-        desc: [
+        content: [
           { required: true, message: '请填写内容', trigger: 'blur' }
         ]
       }
@@ -80,9 +74,16 @@ export default {
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
   methods: {
+    loadChannels () {
+      getArticleChannels().then(res => {
+        this.channels = res.data.data.channels
+      })
+    },
     submitForm (formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
