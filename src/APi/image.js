cc512c015ad50c57e6672c4ed0e4ebd94e8698ad
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-04-28 00:31:20 +0800

    素材管理-展示素材列表

--- src/APi/image.js
+++ src/APi/image.js
@@ -8,3 +8,11 @@ export const uploadImage = data => {
     data
   })
 }
+// 获取素材列表
+export const getImages = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
+  })
+}
