9760846873326ef15d79f1bc3015d2739df1212d
郝斌 <7350648+hao_bin@user.noreply.gitee.com>
2020-04-30 11:14:45 +0800

    评论管理-路由配置和页面创建布局

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -8,6 +8,7 @@
 </el-breadcrumb>
   </div>
   <el-table
+      class="table-list"
       :data="tableData"
       stripe
       style="width: 100%">
@@ -34,23 +35,63 @@
         label="操作">
       </el-table-column>
     </el-table>
+    <!-- 分页 -->
+<el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      background
+></el-pagination>
 </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'CommentIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    }
+  }
 }
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.table-list {
+  margin-bottom: 30px;
+}
+</style>
